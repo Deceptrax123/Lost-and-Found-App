8d946c2f768b1f54c9bab9920a5985962aa3d33e
Srinitish Srinivasan <srinitishsri@gmail.com>
2022-08-23 23:13:06 +0530

    current session

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ if(port==null||port==""||port==undefined){
 }
 app.listen(port,function()
 {
-    console.log("Server running on port " + process.env.PORT);
+    console.log("Server running on port " + port);
 });
 
 require("./config/db")(mongoose);
--- controller/session_routes.js
+++ controller/session_routes.js
@@ -11,13 +11,14 @@ const getCurrentSession=async(req,res)=>{
         let session=await Message.findById(req.params.message_id);
         let owner=await User.findById(session.reciever);
         let finder=await User.findById(session.sender);
+        let item=await Item.findById(session.item_id);
 
         let preference=await Preference.findOne({message_id:req.params.message_id});
-        if(owner==null||finder==null||session==null){
+        if(owner==null||finder==null||session==null||item==null){
             res.status(500).send("Internal Server Error");
         }
         else{
-            res.render("current_session",{session:session,owner:owner,finder:finder,preferences:preference,currentUser:req.user.username});
+            res.render("current_session",{session:session,owner:owner,finder:finder,preferences:preference,currentUser:req.user.username,item:item});
         }
     }catch(err){
         res.status(500).send("Internal Server Error");
